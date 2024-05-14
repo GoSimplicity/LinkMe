c073667a3a0db21cd061c31f754d5559b954c14b
Wang ZiJian <13664854532@163.com>
2024-05-14 20:56:00 +0800

    add jwt

--- wire.go
+++ wire.go
@@ -6,6 +6,7 @@ import (
 	"LinkMe/internal/dao"
 	"LinkMe/internal/repository"
 	"LinkMe/internal/service"
+	ijwt "LinkMe/internal/tools/jwt"
 	"LinkMe/internal/web"
 	"LinkMe/ioc"
 	"github.com/gin-gonic/gin"
@@ -17,6 +18,8 @@ func InitWebServer() *gin.Engine {
 	wire.Build(
 		ioc.InitDB,
 		ioc.InitWebServer,
+		ioc.InitMiddlewares,
+		ijwt.NewJWTHandler,
 		web.NewUserHandler,
 		service.NewUserService,
 		repository.NewUserRepository,
