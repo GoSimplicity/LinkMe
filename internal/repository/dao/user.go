16f57cefc1714e33084d826abf6565efb379f30b
Wang ZiJian <13664854532@163.com>
2024-05-16 16:49:03 +0800

    修改目录结构

--- internal/repository/dao/user.go
+++ internal/repository/dao/user.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	. "LinkMe/internal/repository/models"
 	"context"
 	"errors"
 	"github.com/go-sql-driver/mysql"
@@ -28,19 +29,6 @@ func NewUserDAO(db *gorm.DB) UserDAO {
 	return &userDAO{
 		db: db,
 	}
-}
-
-type User struct {
-	ID           int64      `gorm:"primarykey"`
-	CreateTime   int64      `gorm:"column:created_at;type:bigint"`
-	UpdatedTime  int64      `gorm:"column:updated_at;type:bigint"`
-	DeletedTime  int64      `gorm:"column:deleted_at;type:bigint;index"`
-	Nickname     string     `gorm:"size:50"`
-	PasswordHash string     `gorm:"not null"`
-	Birthday     *time.Time `gorm:"column:birthday;type:datetime"`
-	Email        string     `gorm:"type:varchar(100);uniqueIndex"`
-	Phone        *string    `gorm:"type:varchar(15);uniqueIndex"`
-	About        string     `gorm:"type=varchar(4096)"`
 }
 
 // CreateUser 创建用户
