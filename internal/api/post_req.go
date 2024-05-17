389ce05b8f446ab5bff41b474d9da8b442b21067
Kirito <2493381254@qq.com>
2024-05-17 23:10:47 +0800

    添加发布和撤销接口

--- internal/api/post_req.go
+++ internal/api/post_req.go
@@ -5,3 +5,14 @@ type EditReq struct {
 	Title   string `json:"title,omitempty"`
 	Content string `json:"content,omitempty"`
 }
+
+type PublishReq struct {
+	PostId int64 `json:"postId,omitempty"`
+}
+
+type WithDrawReq struct {
+	PostId int64 `json:"postId,omitempty"`
+}
+
+type ListReq struct {
+}
