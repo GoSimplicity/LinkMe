e2ddd44ba1302315a709ec9e6b8235505d355a67
Wang ZiJian <13664854532@163.com>
2024-05-21 19:33:08 +0800

    添加帖子缓存

--- internal/domain/post.go
+++ internal/domain/post.go
@@ -24,6 +24,7 @@ type Post struct {
 type Pagination struct {
 	Page int    // 当前页码
 	Size *int64 // 每页数据量
+	Uid  int64
 	// 以下字段通常在服务端内部使用，不需要客户端传递
 	Offset *int64 // 数据偏移量
 	Total  *int64 // 总数据量
@@ -37,3 +38,13 @@ const (
 	Withdrawn = "Withdrawn" // 撤回状态
 	Deleted   = "Deleted"   // 删除状态
 )
+
+func (p Post) Abstract() string {
+	// 将Content转换为一个rune切片
+	str := []rune(p.Content)
+	if len(str) > 128 {
+		// 只保留前128个字符作为摘要
+		str = str[:128]
+	}
+	return string(str)
+}
