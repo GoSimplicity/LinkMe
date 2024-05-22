55d2c5738acdbcf605b2577251b0a2aff2793e48
Wang ZiJian <13664854532@163.com>
2024-05-22 18:29:25 +0800

    添加删除接口

---
+++ internal/api/user_test.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"strings"
+)
+
+// Split 把字符串s按照给定的分隔符sep进行分割返回字符串切片
+func Split(s, sep string) (result []string) {
+	i := strings.Index(s, sep)
+
+	for i > -1 {
+		result = append(result, s[:i])
+		s = s[i+len(sep):] // 这里使用len(sep)获取sep的长度
+		i = strings.Index(s, sep)
+	}
+	result = append(result, s)
+	return
+}
+
+//func TestSplit(t *testing.T) {
+//	tests := []struct{
+//		name
+//	}
+//}
