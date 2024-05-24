3160b085131814181a3488b13cb2eb42e29b0cec
Wang ZiJian <13664854532@163.com>
2024-05-24 16:32:06 +0800

    添加注册接口单元测试

--- internal/api/user_test.go
+++ internal/api/user_test.go
@@ -1,44 +1,188 @@
 package api
 
 import (
-	"reflect"
-	"strings"
+	"LinkMe/internal/constants"
+	"LinkMe/internal/domain"
+	"LinkMe/internal/service"
+	svcmocks "LinkMe/internal/service/mocks"
+	"LinkMe/pkg/ginp"
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func Split(s, sep string) (result []string) {
-	i := strings.Index(s, sep)
-
-	for i > -1 {
-		result = append(result, s[:i])
-		s = s[i+len(sep):] // 这里使用len(sep)获取sep的长度
-		i = strings.Index(s, sep)
-	}
-	result = append(result, s)
-	return
-}
-func TestSplitAll(t *testing.T) {
-	// 定义测试表格
-	// 这里使用匿名结构体定义了若干个测试用例
-	// 并且为每个测试用例设置了一个名称
-	tests := []struct {
-		name  string
-		input string
-		sep   string
-		want  []string
+func TestUserHandler_SignUp(t *testing.T) {
+	testCases := []struct {
+		name       string
+		mock       func(ctrl *gomock.Controller) service.UserService
+		reqBuilder func(t *testing.T) *http.Request
+		wantCode   int
+		wantBody   ginp.Result
 	}{
-		{"base case", "a:b:c", ":", []string{"a", "b", "c"}},
-		{"wrong sep", "a:b:c", ",", []string{"a:b:c"}},
-		{"more sep", "abcd", "bc", []string{"a", "d"}},
-		{"leading sep", "沙河有沙又有河", "沙", []string{"", "河有", "又有河"}},
+		{
+			name:     "注册成功",
+			wantCode: 200,
+			wantBody: ginp.Result{
+				Code: http.StatusOK,
+				Msg:  "注册成功",
+			},
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				userSvc := svcmocks.NewMockUserService(ctrl)
+				userSvc.EXPECT().
+					SignUp(gomock.Any(), domain.User{
+						Email:    "1234@qq.com",
+						Password: "Abc@1234",
+					}).Return(nil)
+				return userSvc
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewReader([]byte(`{
+					"email": "1234@qq.com",
+					"password": "Abc@1234",
+					"confirmPassword": "Abc@1234"
+				}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+				return req
+			},
+		},
+		{
+			name:     "无效的请求负载",
+			wantCode: http.StatusBadRequest,
+			wantBody: ginp.Result{
+				Msg: "无效的请求负载",
+			},
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				userSvc := svcmocks.NewMockUserService(ctrl)
+				return userSvc
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewReader([]byte(`{
+				"email": "1234@qq.com",
+				"password": "Abc@1234",
+			}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+				return req
+			},
+		},
+		{
+			name:     "非法邮箱格式",
+			wantCode: http.StatusOK,
+			wantBody: ginp.Result{
+				Code: constants.UserInvalidInput,
+				Msg:  "账号注册失败,请检查邮箱格式",
+			},
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				userSvc := svcmocks.NewMockUserService(ctrl)
+				return userSvc
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewReader([]byte(`{
+				"email": "123",
+				"password": "Abc@1234",
+				"confirmPassword": "Abc@1234"
+			}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+				return req
+			},
+		},
+		{
+			name:     "两次输入密码不对",
+			wantCode: http.StatusOK,
+			wantBody: ginp.Result{
+				Code: constants.UserInvalidInput,
+				Msg:  "输入的两次密码不同,请重新输入",
+			},
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				userSvc := svcmocks.NewMockUserService(ctrl)
+				return userSvc
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewReader([]byte(`{
+				"email": "1234@qq.com",
+				"password": "Abc@1234",
+				"confirmPassword": "Abc@12345"
+			}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+				return req
+			},
+		},
+		{
+			name:     "密码必须包含字母、数字、特殊字符，并且不少于八位",
+			wantCode: http.StatusOK,
+			wantBody: ginp.Result{
+				Code: constants.UserInvalidInput,
+				Msg:  "密码必须包含字母、数字、特殊字符，并且不少于八位",
+			},
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				userSvc := svcmocks.NewMockUserService(ctrl)
+				return userSvc
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewReader([]byte(`{
+				"email": "1234@qq.com",
+				"password": "123",
+				"confirmPassword": "123"
+			}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+				return req
+			},
+		},
+		{
+			name:     "邮箱冲突",
+			wantCode: 200,
+			wantBody: ginp.Result{
+				Code: constants.UserDuplicateEmail,
+				Msg:  "邮箱冲突",
+			},
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				userSvc := svcmocks.NewMockUserService(ctrl)
+				userSvc.EXPECT().SignUp(gomock.Any(), domain.User{
+					Email:    "1234@qq.com",
+					Password: "Abc@1234",
+				}).Return(service.ErrDuplicateEmail)
+				return userSvc
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost, "/users/signup", bytes.NewReader([]byte(`{
+				"email": "1234@qq.com",
+				"password": "Abc@1234",
+				"confirmPassword": "Abc@1234"
+			}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+				return req
+			},
+		},
 	}
-	// 遍历测试用例
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) { // 使用t.Run()执行子测试
-			got := Split(tt.input, tt.sep)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("expected:%#v, got:%#v", tt.want, got)
-			}
+	// 遍历所有测试场景并执行测试
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			userSvc := tc.mock(ctrl)
+			hdl := NewUserHandler(userSvc, nil, nil)
+			server := gin.Default()
+			hdl.RegisterRoutes(server)
+			req := tc.reqBuilder(t)
+			// 创建httptest.Recorder以捕获服务器响应
+			recorder := httptest.NewRecorder()
+			// 发送请求到服务器并接收响应
+			server.ServeHTTP(recorder, req)
+			// 对响应进行断言，验证状态码和响应体是否符合预期
+			assert.Equal(t, tc.wantCode, recorder.Code)
+			wantBody, err := json.Marshal(tc.wantBody)
+			assert.NoError(t, err)
+			assert.Equal(t, string(wantBody), recorder.Body.String())
 		})
 	}
 }
