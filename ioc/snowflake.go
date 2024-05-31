b8923a840fd965f69d20e56668eb968e0b5e975a
Forrest-Tao <2674978072@qq.com>
2024-05-31 20:42:31 +0800

    固定startTime，防止id生成冲突

--- ioc/snowflake.go
+++ ioc/snowflake.go
@@ -7,11 +7,27 @@ import (
 
 // InitializeSnowflakeNode 初始化雪花节点
 func InitializeSnowflakeNode() *sf.Node {
-	st := time.Now()                   // 直接使用当前时间
+	st := getTime()
 	sf.Epoch = st.UnixNano() / 1000000 // 计算起始时间戳并赋值给sf.Epoch
 	node, err := sf.NewNode(1)         // 创建新的节点
 	if err != nil {
 		return nil
 	}
 	return node
+}
+
+func getTime() time.Time {
+	year := 2024
+	month := time.May
+	day := 13
+	hour := 10
+	minute := 30
+	second := 0
+	nanosecond := 0
+	// 加载北京时间 (CST) 时区
+	location, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		panic(err)
+	}
+	return time.Date(year, month, day, hour, minute, second, nanosecond, location)
 }
