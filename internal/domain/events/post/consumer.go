d69c25fa16123be1eefe0d23e35d7551958aad56
Wang ZiJian <13664854532@163.com>
2024-05-31 16:54:02 +0800

    添加互动信息缓存模块
    Former-commit-id: ae0bd6f6bcb2392d6252f728e6af3b58106086c8

--- internal/domain/events/post/consumer.go
+++ internal/domain/events/post/consumer.go
@@ -20,20 +20,46 @@ func NewInteractiveReadEventConsumer(repo repository.InteractiveRepository,
 	return &InteractiveReadEventConsumer{repo: repo, client: client, l: l}
 }
 
+//	func (i *InteractiveReadEventConsumer) Start() error {
+//		cg, err := sarama.NewConsumerGroupFromClient("interactive", i.client)
+//		if err != nil {
+//			return err
+//		}
+//		go func() {
+//			er := cg.Consume(context.Background(),
+//				[]string{TopicReadEvent},
+//				samarap.NewBatchHandler[ReadEvent](i.l, i.BatchConsume))
+//			if er != nil {
+//				i.l.Error("退出消费", zap.Error(er))
+//			}
+//		}()
+//		return err
+//	}
 func (i *InteractiveReadEventConsumer) Start() error {
 	cg, err := sarama.NewConsumerGroupFromClient("interactive", i.client)
 	if err != nil {
 		return err
 	}
 	go func() {
-		er := cg.Consume(context.Background(),
-			[]string{TopicReadEvent},
-			samarap.NewBatchHandler[ReadEvent](i.l, i.BatchConsume))
-		if er != nil {
-			i.l.Error("退出消费", zap.Error(er))
+		attempts := 0
+		maxRetries := 3
+		for attempts < maxRetries {
+			er := cg.Consume(context.Background(),
+				[]string{TopicReadEvent},
+				samarap.NewBatchHandler[ReadEvent](i.l, i.BatchConsume))
+			if er != nil {
+				i.l.Error("消费错误", zap.Error(er), zap.Int("重试次数", attempts+1))
+				attempts++
+				time.Sleep(time.Second * time.Duration(attempts)) // 退避策略，每次重试后等待的时间增加
+				continue
+			}
+			break
+		}
+		if attempts >= maxRetries {
+			i.l.Error("达到最大重试次数，退出消费")
 		}
 	}()
-	return err
+	return nil
 }
 
 func (i *InteractiveReadEventConsumer) BatchConsume(msgs []*sarama.ConsumerMessage,
--- internal/repository/interactive.go
+++ internal/repository/interactive.go
@@ -3,10 +3,13 @@ package repository
 import (
 	. "LinkMe/internal/constants"
 	"LinkMe/internal/domain"
+	"LinkMe/internal/repository/cache"
 	"LinkMe/internal/repository/dao"
 	"LinkMe/internal/repository/models"
 	"context"
 	"errors"
+	"sync"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -25,59 +28,101 @@ type InteractiveRepository interface {
 }
 
 type CachedInteractiveRepository struct {
-	dao dao.InteractiveDAO
-	l   *zap.Logger
+	dao   dao.InteractiveDAO
+	l     *zap.Logger
+	cache cache.InteractiveCache
 }
 
-func NewInteractiveRepository(dao dao.InteractiveDAO, l *zap.Logger) InteractiveRepository {
-	return &CachedInteractiveRepository{dao: dao, l: l}
+func NewInteractiveRepository(dao dao.InteractiveDAO, l *zap.Logger, cache cache.InteractiveCache) InteractiveRepository {
+	return &CachedInteractiveRepository{
+		dao:   dao,
+		l:     l,
+		cache: cache,
+	}
 }
 
 func (c *CachedInteractiveRepository) BatchIncrReadCnt(ctx context.Context, biz []string, ids []int64) error {
 	if err := c.dao.BatchIncrReadCnt(ctx, biz, ids); err != nil {
+		c.l.Error("batch incr read count failed", zap.Error(err))
 		return err
 	}
+	// 使用sync.WaitGroup来等待所有缓存更新操作完成
+	var wg sync.WaitGroup
+	wg.Add(len(biz))
+	for i := 0; i < len(biz); i++ {
+		go func(i int) {
+			defer wg.Done()
+			er := c.retryUpdateCache(ctx, biz[i], ids[i], 3)
+			if er != nil {
+				c.l.Error("post read count record failed", zap.Error(er))
+			}
+		}(i)
+	}
+	wg.Wait()
 	return nil
 }
 
 func (c *CachedInteractiveRepository) IncrLike(ctx context.Context, biz string, id int64, uid int64) error {
-	return c.dao.InsertLikeInfo(ctx, models.UserLikeBiz{
+	if err := c.dao.InsertLikeInfo(ctx, models.UserLikeBiz{
 		BizName: biz,
 		BizID:   id,
 		Uid:     uid,
-	})
+	}); err != nil {
+		c.l.Error("incr like filed", zap.Error(err))
+		return err
+	}
+	return c.cache.PostReadCountRecord(ctx, biz, id)
 }
 
 func (c *CachedInteractiveRepository) DecrLike(ctx context.Context, biz string, id int64, uid int64) error {
-	return c.dao.DeleteLikeInfo(ctx, models.UserLikeBiz{
+	if err := c.dao.DeleteLikeInfo(ctx, models.UserLikeBiz{
 		BizName: biz,
 		BizID:   id,
 		Uid:     uid,
-	})
+	}); err != nil {
+		c.l.Error("decr like filed", zap.Error(err))
+		return err
+	}
+	return c.cache.DecrLikeCountRecord(ctx, biz, id)
 }
 
 func (c *CachedInteractiveRepository) IncrCollectionItem(ctx context.Context, biz string, id int64, cid int64, uid int64) error {
-	return c.dao.InsertCollectionBiz(ctx, models.UserCollectionBiz{
+	if err := c.dao.InsertCollectionBiz(ctx, models.UserCollectionBiz{
 		BizName:      biz,
 		BizID:        id,
 		CollectionId: cid,
 		Uid:          uid,
-	})
+	}); err != nil {
+		c.l.Error("incr collection item filed", zap.Error(err))
+		return err
+	}
+	return c.cache.PostCollectCountRecord(ctx, biz, id)
 }
 func (c *CachedInteractiveRepository) DecrCollectionItem(ctx context.Context, biz string, id int64, cid int64, uid int64) error {
-	return c.dao.DeleteCollectionBiz(ctx, models.UserCollectionBiz{
+	if err := c.dao.DeleteCollectionBiz(ctx, models.UserCollectionBiz{
 		BizName:      biz,
 		BizID:        id,
 		CollectionId: cid,
 		Uid:          uid,
-	})
+	}); err != nil {
+		c.l.Error("decr collection item filed", zap.Error(err))
+		return err
+	}
+	return c.cache.DecrCollectCountRecord(ctx, biz, id)
 }
 
 func (c *CachedInteractiveRepository) Get(ctx context.Context, biz string, id int64) (domain.Interactive, error) {
+	inc, err := c.cache.Get(ctx, biz, id)
+	if err == nil {
+		return inc, nil
+	}
 	ic, err := c.dao.Get(ctx, biz, id)
 	if err != nil {
 		c.l.Error(PostGetInteractiveERROR, zap.Error(err))
 		return domain.Interactive{}, err
+	}
+	if er := c.cache.Set(ctx, biz, id, toDomain(ic)); er != nil {
+		c.l.Error("set interactive cache filed", zap.Error(er))
 	}
 	return toDomain(ic), nil
 }
@@ -126,6 +171,18 @@ func (c *CachedInteractiveRepository) GetById(ctx context.Context, biz string, i
 	}
 
 	return result, nil
+}
+
+func (c *CachedInteractiveRepository) retryUpdateCache(ctx context.Context, biz string, id int64, retries int) error {
+	for i := 0; i < retries; i++ {
+		err := c.cache.PostReadCountRecord(ctx, biz, id)
+		if err == nil {
+			return nil // 更新成功，返回
+		}
+		// 如果更新失败，等待一段时间后再重试
+		time.Sleep(time.Millisecond * 100) // 等待100毫秒
+	}
+	return errors.New("failed to update cache after retries")
 }
 
 func toDomain(ic models.Interactive) domain.Interactive {
