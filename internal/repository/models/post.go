20e0a7cc07eedfa600e8d62245e726c0bbba16ef
Wang ZiJian <13664854532@163.com>
2024-06-01 15:52:13 +0800

    添加文档/注释

--- internal/repository/models/post.go
+++ internal/repository/models/post.go
@@ -15,4 +15,3 @@ type Post struct {
 	Tags         string `gorm:"type:varchar(255);default:''"` // 文章标签，以逗号分隔
 	CommentCount int64  `gorm:"default:0"`                    // 文章的评论数量
 }
-type PublishedPost Post
