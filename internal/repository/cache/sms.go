c6ef41bfcab8843807191ff7ea5229af3e037df1
Forrest-Tao <2674978072@qq.com>
2024-06-04 21:24:17 +0800

    修改sms v1.0

---
+++ internal/repository/cache/sms.go
@@ -0,0 +1,40 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+type SMSCache interface {
+	GetVCode(ctx context.Context, smsID, mobile string) string
+	StoreVCode(ctx context.Context, smsID, mobile string, vCode string)
+}
+
+type smsCache struct {
+	client redis.Cmdable
+}
+
+func NewSMSCache(client redis.Cmdable) SMSCache {
+	return &smsCache{
+		client: client,
+	}
+}
+
+const VCodeKey = "sms:%s:%s"
+
+func getVCodeKey(smsID, mobile string) string {
+	return fmt.Sprintf(VCodeKey, smsID, mobile)
+}
+
+func (s smsCache) GetVCode(ctx context.Context, smsID, mobile string) string {
+	key := getVCodeKey(smsID, mobile)
+	vCode, _ := s.client.Get(ctx, key).Result()
+	return vCode
+}
+
+func (s smsCache) StoreVCode(ctx context.Context, smsID, mobile string, vCode string) {
+	key := getVCodeKey(smsID, mobile)
+	s.client.Set(ctx, key, vCode, time.Minute*10)
+}
