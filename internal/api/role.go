ee4169e89dfcd9c762e705de6716ee250828341c
Wang ZiJian <13664854532@163.com>
2024-07-01 11:55:12 +0800

    完善权限模块/新增部分接口

--- internal/api/role.go
+++ internal/api/role.go
@@ -4,7 +4,6 @@ import (
 	"LinkMe/internal/service"
 	"LinkMe/middleware"
 	. "LinkMe/pkg/ginp"
-	"LinkMe/utils/jwt"
 	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -29,21 +28,16 @@ func (h *PermissionHandler) RegisterRoutes(server *gin.Engine) {
 	casbinMiddleware := middleware.NewCasbinMiddleware(h.ce, h.l)
 	permissionGroup := server.Group("/permissions")
 	permissionGroup.Use(casbinMiddleware.CheckCasbin())
-	permissionGroup.GET("/list", WrapBody(h.GetPermissions))        // 获取权限列表
-	permissionGroup.POST("/assign", WrapBody(h.AssignPermission))   // 分配权限
-	permissionGroup.DELETE("/remove", WrapBody(h.RemovePermission)) // 移除权限
+	permissionGroup.GET("/list", WrapQuery(h.GetPermissions))                // 获取权限列表
+	permissionGroup.POST("/assign", WrapBody(h.AssignPermission))            // 分配权限
+	permissionGroup.POST("/assign_role", WrapBody(h.AssignRole))             // 分配角色
+	permissionGroup.DELETE("/remove", WrapBody(h.RemovePermission))          // 移除权限
+	permissionGroup.DELETE("/remove_role", WrapBody(h.RemovePermissionRole)) // 移除角色
 }
 
 // GetPermissions 处理获取权限列表的请求
 func (h *PermissionHandler) GetPermissions(ctx *gin.Context, req ListPermissionsReq) (Result, error) {
-	uc := ctx.MustGet("user").(jwt.UserClaims)
-	if uc.Uid != req.UserID {
-		return Result{
-			Code: http.StatusBadRequest,
-			Msg:  "get permissions failed",
-		}, nil
-	}
-	permissions, err := h.svc.GetPermissions(ctx, uc.Uid)
+	permissions, err := h.svc.GetPermissions(ctx)
 	if err != nil {
 		return Result{
 			Code: http.StatusInternalServerError,
@@ -59,7 +53,7 @@ func (h *PermissionHandler) GetPermissions(ctx *gin.Context, req ListPermissions
 
 // AssignPermission 处理分配权限的请求
 func (h *PermissionHandler) AssignPermission(ctx *gin.Context, req AssignPermissionReq) (Result, error) {
-	if err := h.svc.AssignPermission(ctx, req.UserID, req.Path, req.Method); err != nil {
+	if err := h.svc.AssignPermission(ctx, req.UserName, req.Path, req.Method); err != nil {
 		return Result{
 			Code: http.StatusInternalServerError,
 			Msg:  "assign permission failed",
@@ -73,7 +67,7 @@ func (h *PermissionHandler) AssignPermission(ctx *gin.Context, req AssignPermiss
 
 // RemovePermission 处理移除权限的请求
 func (h *PermissionHandler) RemovePermission(ctx *gin.Context, req RemovePermissionReq) (Result, error) {
-	if err := h.svc.RemovePermission(ctx, req.UserID, req.Path, req.Method); err != nil {
+	if err := h.svc.RemovePermission(ctx, req.UserName, req.Path, req.Method); err != nil {
 		return Result{
 			Code: http.StatusInternalServerError,
 			Msg:  "remove permission failed",
@@ -82,5 +76,31 @@ func (h *PermissionHandler) RemovePermission(ctx *gin.Context, req RemovePermiss
 	return Result{
 		Code: http.StatusOK,
 		Msg:  "remove permission success",
+	}, nil
+}
+
+func (h *PermissionHandler) AssignRole(ctx *gin.Context, req AssignPermissionRoleReq) (Result, error) {
+	if err := h.svc.AssignRoleToUser(ctx, req.UserName, req.RoleName); err != nil {
+		return Result{
+			Code: http.StatusInternalServerError,
+			Msg:  "assign role failed",
+		}, err
+	}
+	return Result{
+		Code: http.StatusOK,
+		Msg:  "assign role success",
+	}, nil
+}
+
+func (h *PermissionHandler) RemovePermissionRole(ctx *gin.Context, req RemovePermissionRoleReq) (Result, error) {
+	if err := h.svc.RemoveRoleFromUser(ctx, req.UserName, req.RoleName); err != nil {
+		return Result{
+			Code: http.StatusInternalServerError,
+			Msg:  "remove role failed",
+		}, err
+	}
+	return Result{
+		Code: http.StatusOK,
+		Msg:  "remove role success",
 	}, nil
 }
