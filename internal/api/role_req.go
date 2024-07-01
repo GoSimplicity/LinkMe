be66784f03cf1f236364088521e7e87ab58b6035
Wang ZiJian <13664854532@163.com>
2024-07-01 11:55:12 +0800

    完善权限模块/新增部分接口

--- internal/api/role_req.go
+++ internal/api/role_req.go
@@ -2,19 +2,30 @@ package api
 
 // ListPermissionsReq 是获取权限列表的请求
 type ListPermissionsReq struct {
-	UserID int64 `json:"userId,omitempty"` // 用户ID
 }
 
 // AssignPermissionReq 是分配权限的请求
 type AssignPermissionReq struct {
-	UserID int64  `json:"userId,omitempty"` // 用户ID
-	Path   string `json:"path,omitempty"`   // 接口路径
-	Method string `json:"method,omitempty"` // 操作类型
+	UserName string `json:"userName,omitempty"` // 用户
+	Path     string `json:"path,omitempty"`     // 接口路径
+	Method   string `json:"method,omitempty"`   // 操作类型
+}
+
+// AssignPermissionRoleReq 是分配角色的请求
+type AssignPermissionRoleReq struct {
+	UserName string `json:"userName,omitempty"` // 用户
+	RoleName string `json:"roleName,omitempty"` // 角色
 }
 
 // RemovePermissionReq 是移除权限的请求
 type RemovePermissionReq struct {
-	UserID int64  `json:"userId,omitempty"` // 用户ID
-	Path   string `json:"path,omitempty"`   // 接口路径
-	Method string `json:"method,omitempty"` // 操作类型
+	UserName string `json:"userName,omitempty"` // 用户
+	Path     string `json:"path,omitempty"`     // 接口路径
+	Method   string `json:"method,omitempty"`   // 操作类型
+}
+
+// RemovePermissionRoleReq 是移除角色的请求
+type RemovePermissionRoleReq struct {
+	UserName string `json:"userName,omitempty"` // 用户
+	RoleName string `json:"roleName,omitempty"` // 角色
 }
