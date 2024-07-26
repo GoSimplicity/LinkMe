7b6b06417c82e437812673c134ccea4f66c850e9
Wang ZiJian <13664854532@163.com>
2024-07-26 16:36:24 +0800

    完成评论功能部分接口

--- internal/service/comment.go
+++ internal/service/comment.go
@@ -6,39 +6,44 @@ import (
 	"context"
 )
 
+// 评论服务结构体
 type commentService struct {
 	repo repository.CommentRepository
 }
 
+// 评论服务接口
 type CommentService interface {
 	CreateComment(ctx context.Context, comment domain.Comment) error
 	DeleteComment(ctx context.Context, commentId int64) error
-	ListComment(ctx context.Context, Pagination domain.Pagination) ([]domain.Comment, error)
-	GetMoreCommentReply(ctx context.Context, commentId int64, pagination domain.Pagination, Id int64) ([]domain.Comment, error)
+	ListComments(ctx context.Context, biz string, bizID, minID, limit int64) ([]domain.Comment, error)
+	GetMoreCommentsReply(ctx context.Context, commentId int64, pagination domain.Pagination, Id int64) ([]domain.Comment, error)
 }
 
+// 创建新的评论服务
 func NewCommentService(repo repository.CommentRepository) CommentService {
 	return &commentService{
 		repo: repo,
 	}
 }
 
-// CreateComment implements CommentService.
+// CreateComment 创建评论的实现
 func (c *commentService) CreateComment(ctx context.Context, comment domain.Comment) error {
-	panic("unimplemented")
+	// 实现创建评论的逻辑
+	return c.repo.CreateComment(ctx, comment)
 }
 
-// DeleteComment implements CommentService.
+// DeleteComment 删除评论的实现
 func (c *commentService) DeleteComment(ctx context.Context, commentId int64) error {
-	panic("unimplemented")
+	return c.repo.DeleteComment(ctx, commentId)
 }
 
-// GetMoreCommentReply implements CommentService.
-func (c *commentService) GetMoreCommentReply(ctx context.Context, commentId int64, pagination domain.Pagination, Id int64) ([]domain.Comment, error) {
-	panic("unimplemented")
+// GetMoreCommentsReply 获取更多评论回复的实现
+func (c *commentService) GetMoreCommentsReply(ctx context.Context, commentId int64, pagination domain.Pagination, Id int64) ([]domain.Comment, error) {
+	// 实现获取更多评论回复的逻辑
+	return c.repo.GetMoreCommentReply(ctx, commentId, pagination, Id)
 }
 
-// ListComment implements CommentService.
-func (c *commentService) ListComment(ctx context.Context, Pagination domain.Pagination) ([]domain.Comment, error) {
-	panic("unimplemented")
+// ListComments 列出评论的实现
+func (c *commentService) ListComments(ctx context.Context, biz string, bizID, minID, limit int64) ([]domain.Comment, error) {
+	return c.repo.ListComments(ctx, biz, bizID, minID, limit)
 }
