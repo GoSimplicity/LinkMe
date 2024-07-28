71a01b114a7cae6bcab572bdc758098025e20620
王子健 <3025104405@qq.com>
2024-07-28 19:37:10 +0800

    修改评论模块部分bug

--- internal/api/comment_req.go
+++ internal/api/comment_req.go
@@ -3,22 +3,22 @@ package api
 type CreateCommentReq struct {
 	PostId  int64  `json:"postId" binding:"required"`
 	Content string `json:"content" binding:"required"`
-	RootId  *int64 `json:"root_id,omitempty"` // 根评论ID，顶层评论时为空
-	PID     *int64 `json:"pid,omitempty"`     // 父评论ID，顶层评论时为空
+	RootId  *int64 `json:"rootId,omitempty"` // 根评论ID，顶层评论时为空
+	PID     *int64 `json:"pid,omitempty"`    // 父评论ID，顶层评论时为空
 }
 
 type ListCommentsReq struct {
-	PostId int64
-	MinId  int64
-	Limit  int64
+	PostId int64 `json:"postId"`
+	MinId  int64 `json:"minId"`
+	Limit  int64 `json:"limit"`
 }
 
 type DeleteCommentReq struct {
-	CommentId int64 `json:"commentId" binding:"required"`
+	CommentId int64 `uri:"commentId"`
 }
 
 type GetMoreCommentReplyReq struct {
-	RootId int64
-	MaxId  int64
-	Limit  int64
+	RootId int64 `json:"rootId"`
+	MaxId  int64 `json:"maxId"`
+	Limit  int64 `json:"limit"`
 }
