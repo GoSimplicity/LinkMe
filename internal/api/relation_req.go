91db3027fdf5a794b6edcc22f84b01fcde27906c
王子健 <3025104405@qq.com>
2024-07-30 17:25:55 +0800

    完成用户关系模块

--- internal/api/relation_req.go
+++ internal/api/relation_req.go
@@ -1,10 +1,30 @@
 package api
 
 type ListRelationsReq struct {
+	FollowerID int64  `json:"followerId"`
+	Page       int    `json:"page,omitempty"` // 当前页码
+	Size       *int64 `json:"size,omitempty"` // 每页数据量
 }
 
 type GetRelationInfoReq struct {
+	FollowerID int64 `json:"followerId"`
+	FolloweeID int64 `json:"followeeId"`
 }
 
 type FollowUserReq struct {
+	FollowerID int64 `json:"followerId"`
+	FolloweeID int64 `json:"followeeId"`
+}
+
+type CancelFollowUserReq struct {
+	FollowerID int64 `json:"followerId"`
+	FolloweeID int64 `json:"followeeId"`
+}
+
+type GetFolloweeCountReq struct {
+	UserID int64 `json:"userId"`
+}
+
+type GetFollowerCountReq struct {
+	UserID int64 `json:"userId"`
 }
--- internal/repository/relation.go
+++ internal/repository/relation.go
@@ -12,6 +12,8 @@ type RelationRepository interface {
 	GetRelationInfo(ctx context.Context, followerID, followeeID int64) (domain.Relation, error)
 	FollowUser(ctx context.Context, followerID, followeeID int64) error
 	CancelFollowUser(ctx context.Context, followerID, followeeID int64) error
+	GetFolloweeCount(ctx context.Context, userID int64) (int64, error)
+	GetFollowerCount(ctx context.Context, userID int64) (int64, error)
 }
 
 type relationRepository struct {
@@ -24,25 +26,55 @@ func NewRelationRepository(dao dao.RelationDAO) RelationRepository {
 	}
 }
 
-// ListRelations 列出所有关注关系
+// FollowUser 关注用户
+func (r *relationRepository) FollowUser(ctx context.Context, followerID, followeeID int64) error {
+	return r.dao.FollowUser(ctx, followerID, followeeID)
+}
+
+func (r *relationRepository) CancelFollowUser(ctx context.Context, followerID, followeeID int64) error {
+	return r.dao.CancelFollowUser(ctx, followerID, followeeID)
+}
+
+// ListRelations 列出关注列表
 func (r *relationRepository) ListRelations(ctx context.Context, followerID int64, pagination domain.Pagination) ([]domain.Relation, error) {
-	// TODO 实现方法
-	panic("implement me")
+	relations, err := r.dao.ListRelations(ctx, followerID, pagination)
+	return r.toDomainRelationSlice(relations), err
 }
 
 // GetRelationInfo 获取特定的关注关系信息
 func (r *relationRepository) GetRelationInfo(ctx context.Context, followerID, followeeID int64) (domain.Relation, error) {
-	// TODO 实现方法
-	panic("implement me")
+	info, err := r.dao.GetRelationInfo(ctx, followerID, followeeID)
+	return r.toDomainRelation(info), err
 }
 
-// FollowUser 关注用户
-func (r *relationRepository) FollowUser(ctx context.Context, followerID, followeeID int64) error {
-	// TODO 实现方法
-	panic("implement me")
+func (r *relationRepository) GetFolloweeCount(ctx context.Context, userID int64) (int64, error) {
+	count, err := r.dao.FollowCount(ctx, userID)
+	return count.FolloweeCount, err
 }
 
-func (r *relationRepository) CancelFollowUser(ctx context.Context, followerID, followeeID int64) error {
-	//TODO implement me
-	panic("implement me")
+func (r *relationRepository) GetFollowerCount(ctx context.Context, userID int64) (int64, error) {
+	count, err := r.dao.FollowCount(ctx, userID)
+	return count.FollowerCount, err
+}
+
+func (r *relationRepository) toDomainRelation(relation dao.Relation) domain.Relation {
+	return domain.Relation{
+		FolloweeId: relation.FolloweeID,
+		FollowerId: relation.FollowerID,
+	}
+}
+
+func (r *relationRepository) toDomainRelationSlice(relations []dao.Relation) []domain.Relation {
+	relationSlice := make([]domain.Relation, len(relations))
+	for i, relation := range relations {
+		relationSlice[i] = r.toDomainRelation(relation)
+	}
+	return relationSlice
+}
+
+func (r *relationRepository) toDAORelation(relation dao.Relation) dao.Relation {
+	return dao.Relation{
+		FolloweeID: relation.FolloweeID,
+		FollowerID: relation.FollowerID,
+	}
 }
