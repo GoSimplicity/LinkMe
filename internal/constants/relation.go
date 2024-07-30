201b3c14d140310e5ed0e62ace69ecd5c57c8f58
王子健 <3025104405@qq.com>
2024-07-30 17:25:55 +0800

    完成用户关系模块

--- internal/constants/relation.go
+++ internal/constants/relation.go
@@ -1,8 +1,22 @@
 package constants
 
-/**
- * @Author: Bamboo
- * @Author: 13664854532@163.com
- * @Date: 2024/7/29 16:08
- * @Desc:
- */
+const (
+	CreateRelationERRORCode    = 407001
+	GetRelationERRORCode       = 407002
+	FollowUserERRORCode        = 407003
+	CancelFollowUserERRORCode  = 407004
+	GetFolloweeCountErrorCode  = 407005
+	GetFollowerCountErrorCode  = 407006
+	GetFolloweeCountSuccessMsg = "Followee count retrieved successfully"
+	GetFollowerCountSuccessMsg = "Follower count retrieved successfully"
+	GetFolloweeCountERRORMsg   = "Failed to get followee count"
+	GetFollowerCountERRORMsg   = "Failed to get follower count"
+	CreateRelationERRORMsg     = "Failed to create relation"
+	GetRelationERRORMsg        = "Failed to get relation"
+	FollowUserERRORMsg         = "Failed to follow user"
+	CancelFollowUserERRORMsg   = "Failed to cancel follow user"
+	CreateRelationSuccessMsg   = "Relation created successfully"
+	GetRelationSuccessMsg      = "Relation retrieved successfully"
+	FollowUserSuccessMsg       = "User followed successfully"
+	CancelFollowUserSuccessMsg = "User unfollowed successfully"
+)
