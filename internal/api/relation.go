3422d454cb39b53878625767ae28710cd6716b0d
王子健 <3025104405@qq.com>
2024-07-30 17:25:55 +0800

    完成用户关系模块
    Former-commit-id: 5a21b8988ddb81b08b04aa4e840ccf3ea41af515

--- internal/api/relation.go
+++ internal/api/relation.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	. "github.com/GoSimplicity/LinkMe/internal/constants"
+	"github.com/GoSimplicity/LinkMe/internal/domain"
 	"github.com/GoSimplicity/LinkMe/internal/service"
 	. "github.com/GoSimplicity/LinkMe/pkg/ginp"
 	"github.com/gin-gonic/gin"
@@ -12,9 +14,12 @@ type RelationHandler struct {
 
 func (r *RelationHandler) RegisterRoutes(server *gin.Engine) {
 	relationGroup := server.Group("/api/relations")
-	relationGroup.POST("/list", WrapBody(r.ListRelations))
-	relationGroup.GET("/get_info", WrapQuery(r.GetRelationInfo))
-	relationGroup.POST("/follow", WrapBody(r.FollowUser))
+	relationGroup.POST("/list", WrapBody(r.ListRelations))                  // 查看用户关系列表
+	relationGroup.GET("/get_info", WrapQuery(r.GetRelationInfo))            // 查看用户关系信息
+	relationGroup.GET("/get_followee_count", WrapQuery(r.GetFolloweeCount)) // 获取关注者数量
+	relationGroup.GET("/get_follower_count", WrapQuery(r.GetFollowerCount)) // 获取粉丝数量
+	relationGroup.POST("/follow", WrapBody(r.FollowUser))                   // 关注
+	relationGroup.POST("/cancel_follow", WrapBody(r.CancelFollowUser))      // 关注
 }
 
 func NewRelationHandler(svc service.RelationService) *RelationHandler {
@@ -25,18 +30,94 @@ func NewRelationHandler(svc service.RelationService) *RelationHandler {
 
 // ListRelations 处理列出关注关系的请求
 func (r *RelationHandler) ListRelations(ctx *gin.Context, req ListRelationsReq) (Result, error) {
-	// TODO 实现方法
-	return Result{}, nil
+	relations, err := r.svc.ListRelations(ctx, req.FollowerID, domain.Pagination{
+		Page: req.Page,
+		Size: req.Size,
+	})
+	if err != nil {
+		return Result{
+			Code: ListCommentErrorCode,
+			Msg:  ListCommentErrorMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  ListCommentSuccessMsg,
+		Data: relations,
+	}, nil
 }
 
 // GetRelationInfo 处理获取关注关系信息的请求
 func (r *RelationHandler) GetRelationInfo(ctx *gin.Context, req GetRelationInfoReq) (Result, error) {
-	// TODO 实现方法
-	return Result{}, nil
+	relation, err := r.svc.GetRelationInfo(ctx, req.FollowerID, req.FolloweeID)
+	if err != nil {
+		return Result{
+			Code: ListCommentErrorCode,
+			Msg:  ListCommentErrorMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  ListCommentSuccessMsg,
+		Data: relation,
+	}, nil
 }
 
 // FollowUser 处理关注用户的请求
 func (r *RelationHandler) FollowUser(ctx *gin.Context, req FollowUserReq) (Result, error) {
-	// TODO 实现方法
-	return Result{}, nil
+	if err := r.svc.FollowUser(ctx, req.FollowerID, req.FolloweeID); err != nil {
+		return Result{
+			Code: FollowUserERRORCode,
+			Msg:  FollowUserERRORMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  FollowUserSuccessMsg,
+	}, nil
+}
+
+func (r *RelationHandler) CancelFollowUser(ctx *gin.Context, req CancelFollowUserReq) (Result, error) {
+	if err := r.svc.CancelFollowUser(ctx, req.FollowerID, req.FolloweeID); err != nil {
+		return Result{
+			Code: CancelFollowUserERRORCode,
+			Msg:  CancelFollowUserERRORMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  CancelFollowUserSuccessMsg,
+	}, nil
+}
+
+// GetFolloweeCount 获取关注者的数量
+func (r *RelationHandler) GetFolloweeCount(ctx *gin.Context, req GetFolloweeCountReq) (Result, error) {
+	count, err := r.svc.GetFolloweeCount(ctx, req.UserID)
+	if err != nil {
+		return Result{
+			Code: GetFolloweeCountErrorCode,
+			Msg:  GetFolloweeCountERRORMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  GetFolloweeCountSuccessMsg,
+		Data: count,
+	}, nil
+}
+
+// GetFollowerCount 获取粉丝的数量
+func (r *RelationHandler) GetFollowerCount(ctx *gin.Context, req GetFollowerCountReq) (Result, error) {
+	count, err := r.svc.GetFollowerCount(ctx, req.UserID)
+	if err != nil {
+		return Result{
+			Code: GetFollowerCountErrorCode,
+			Msg:  GetFollowerCountERRORMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  GetFollowerCountSuccessMsg,
+		Data: count,
+	}, nil
 }
