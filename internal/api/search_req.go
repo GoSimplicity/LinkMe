a0b428114c300d376b12a8b0e8d9ac4503c3e4d3
王子健 <3025104405@qq.com>
2024-08-02 17:15:43 +0800

    完成搜索功能

--- internal/api/search_req.go
+++ internal/api/search_req.go
@@ -1,6 +1,5 @@
 package api
 
 type SearchReq struct {
-	expression string
-	userID     int64
+	Expression string `json:"expression"`
 }
--- internal/repository/search.go
+++ internal/repository/search.go
@@ -11,8 +11,8 @@ type searchRepository struct {
 }
 
 type SearchRepository interface {
-	SearchPosts(ctx context.Context, userID int64, expression string) ([]domain.UserSearch, error)
-	SearchUsers(ctx context.Context, userID int64, expression string) ([]domain.PostSearch, error)
+	SearchPosts(ctx context.Context, keywords []string) ([]domain.PostSearch, error)
+	SearchUsers(ctx context.Context, keywords []string) ([]domain.UserSearch, error)
 }
 
 func NewSearchRepository(dao dao.SearchDAO) SearchRepository {
@@ -21,12 +21,39 @@ func NewSearchRepository(dao dao.SearchDAO) SearchRepository {
 	}
 }
 
-func (s searchRepository) SearchPosts(ctx context.Context, userID int64, expression string) ([]domain.UserSearch, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *searchRepository) SearchPosts(ctx context.Context, keywords []string) ([]domain.PostSearch, error) {
+	posts, err := s.dao.SearchPosts(ctx, keywords)
+	return s.toDomainPostSearch(posts), err
 }
 
-func (s searchRepository) SearchUsers(ctx context.Context, userID int64, expression string) ([]domain.PostSearch, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *searchRepository) SearchUsers(ctx context.Context, keywords []string) ([]domain.UserSearch, error) {
+	users, err := s.dao.SearchUsers(ctx, keywords)
+	return s.toDomainUserSearch(users), err
+}
+
+func (s *searchRepository) toDomainPostSearch(daoPosts []dao.PostSearch) []domain.PostSearch {
+	domainPosts := make([]domain.PostSearch, len(daoPosts))
+	for i, daoPost := range daoPosts {
+		domainPosts[i] = domain.PostSearch{
+			Content: daoPost.Content,
+			Id:      daoPost.Id,
+			Status:  daoPost.Status,
+			Tags:    daoPost.Tags,
+			Title:   daoPost.Title,
+		}
+	}
+	return domainPosts
+}
+
+func (s *searchRepository) toDomainUserSearch(daoUsers []dao.UserSearch) []domain.UserSearch {
+	domainUsers := make([]domain.UserSearch, len(daoUsers))
+	for i, daoUser := range daoUsers {
+		domainUsers[i] = domain.UserSearch{
+			Email:    daoUser.Email,
+			Id:       daoUser.Id,
+			Nickname: daoUser.Nickname,
+			Phone:    daoUser.Phone,
+		}
+	}
+	return domainUsers
 }
