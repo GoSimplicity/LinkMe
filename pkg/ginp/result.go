ea20ca2bd50968c5386f6e6b2ef3623ef93f6fc4
Bamboo <13664854532@163.com>
2024-08-23 17:16:18 +0800

    返回token以供前端配置

--- pkg/ginp/result.go
+++ pkg/ginp/result.go
@@ -1,7 +1,14 @@
 package ginp
 
 type Result struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data any    `json:"data"`
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+type TokenResult struct {
+	Code         int    `json:"code"`
+	Msg          string `json:"msg"`
+	JWTToken     string `json:"jwt_token"`
+	RefreshToken string `json:"refresh_token"`
 }
