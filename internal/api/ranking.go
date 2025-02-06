2f1171437b6a7761543194a49896d894d5f4a911
Penge666 <1280032578@qq.com>
2025-02-06 16:51:15 +0800

    完成榜单规则设计以及重置，同时重写热榜算法

--- internal/api/ranking.go
+++ internal/api/ranking.go
@@ -1,8 +1,12 @@
 package api
 
 import (
+	"github.com/GoSimplicity/LinkMe/internal/api/req"
+	. "github.com/GoSimplicity/LinkMe/internal/constants"
+	"github.com/GoSimplicity/LinkMe/internal/domain"
 	"github.com/GoSimplicity/LinkMe/internal/service"
 	"github.com/GoSimplicity/LinkMe/pkg/apiresponse"
+	. "github.com/GoSimplicity/LinkMe/pkg/ginp"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,6 +24,8 @@ func (rh *RankingHandler) RegisterRoutes(server *gin.Engine) {
 	postGroup := server.Group("/api/raking")
 
 	postGroup.GET("/topN", rh.GetRanking)
+	postGroup.GET("/config", rh.GetRankingConfig)
+	postGroup.POST("/reset", WrapBody(rh.ResetRanking))
 }
 
 // GetRanking 获取排行榜
@@ -29,6 +35,34 @@ func (rh *RankingHandler) GetRanking(ctx *gin.Context) {
 		apiresponse.ErrorWithData(ctx, err)
 		return
 	}
-
 	apiresponse.SuccessWithData(ctx, dp)
+}
+
+func (rh *RankingHandler) GetRankingConfig(ctx *gin.Context) {
+	dp, err := rh.svc.GetRankingConfig(ctx)
+	if err != nil {
+		apiresponse.ErrorWithData(ctx, err)
+		return
+	}
+	apiresponse.SuccessWithData(ctx, dp)
+}
+
+func (rh *RankingHandler) ResetRanking(ctx *gin.Context, req req.RankingParameterReq) (Result, error) {
+	rankingParameters := domain.RankingParameter{
+		Alpha:  req.Alpha,
+		Beta:   req.Beta,
+		Gamma:  req.Gamma,
+		Lambda: req.Lambda,
+	}
+	err := rh.svc.ResetRankingConfig(ctx, rankingParameters)
+	if err != nil {
+		return Result{
+			Code: SetRankingErrorCode,
+			Msg:  SetRankingErrorMsg,
+		}, err
+	}
+	return Result{
+		Code: RequestsOK,
+		Msg:  SetRankingSuccessMsg,
+	}, nil
 }
